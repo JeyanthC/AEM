7617c01c6456605b749df7b04633e6aaf2ce48ed
Jeyanth C <35778904+JeyanthC@users.noreply.github.com>
2018-10-04 21:59:36 +0530

    Updated ldr_interface.py

--- ldr_interface.py
+++ ldr_interface.py
@@ -8,10 +8,9 @@ GPIO.setup(4,GPIO.IN)
 ldr_count = 0
 no_of_units = 0
 
-for i in range(0,5):
-    print GPIO.input(4)
-    #ldr_count  = ldr_count + 1
-
-#if ldr_count == 3200:
-#   no_of_units = no_of_units + 1
+while(1):
+    ldr_count  = ldr_count + 1
+    if ldr_count == 3200:
+        no_of_units = no_of_units + 1
+        ldr_count = 0
     
